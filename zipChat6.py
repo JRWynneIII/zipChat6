444331dc068090709a0f62256315515adea7de19
James Riley Wynne III <wynnejr@ornl.gov>
2014-09-26 17:06:54 -0400

    Added comments to clarify code

--- zipChat6.py
+++ zipChat6.py
@@ -7,6 +7,10 @@ import hashlib
 import netifaces as ni
 import numpy
 
+#
+#   Base server class. 
+#   Defaults to the UDP protocol and port 10008 unless otherwise specified
+#
 class zServer:
     def __init__(self,port=10008,proto="UDP"):
         self.port = port
@@ -26,10 +30,12 @@ class zServer:
         tup[0] = me
         tup = tuple(tup)
         self.addrTup = tup
-
+    
+    #Returns the IPv6 Address that was found when the constructor (__init__) was run
     def getIP6Addr(self):
         return self.addrTup
 
+    #Sends `data` to the specified port and address (destination); UDP
     def Send(self,destination,port,data):
         try:
             s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
@@ -37,11 +43,16 @@ class zServer:
         except Exception as e:
             print("Error: ", e)
 
+#
+#   Base client class
+#   Defaults to UDP protocol
+#
 class zClient:
     def __init__(self):
         if not socket.has_ipv6:
             raise Exception("The local machine has no IPv6 support enabled")
     
+    #UDP listener. 
     def listen(self,address,port):
         try:
             s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
@@ -52,7 +63,12 @@ class zClient:
         except Exception as e:
             print("Error: ", e)
 
-
+#
+#   Heartbeat Server will send out a "i'm alive/online" signal every 2 minutes. 
+#   The data sent can include things like status messages and should end in a checksum 
+#   of the "packet". Checksum not yet implemented.
+#   Heartbeat is sent out on port 10009
+#
 class heartbeatServer:
     def __init__(self):
         self.port = 10009
@@ -62,7 +78,8 @@ class heartbeatServer:
  
         if len(addrs) == 0:
             raise Exception("there is no IPv6 address configured for localhost")
-    
+
+    #Gets the IPv6 address of the 'tun0' (CJDNS) device
     def getAddr(self):
         me=ni.ifaddresses('tun0')[10][0]['addr']
         addrs = socket.getaddrinfo("localhost", 10009, socket.AF_INET6, 0, socket.SOL_TCP)
@@ -73,10 +90,12 @@ class heartbeatServer:
         tup = tuple(tup)
         return tup
 
+    #Starts the heartbeat on a seperate thread. Will send out heartbeat packet to every IP in `ipList`
     def start(self,port,ipList):
         t = threading.Thread(target=self.beat, args=(port,ipList,))
         t.start()
 
+    #Builds the heartbeat packet to be sent
     def build_heartbeat_packet(self,serv):
         try:
             packet = []
@@ -88,7 +107,7 @@ class heartbeatServer:
         except Exception as e:
             print("Error: ", e);
 
-    #CHANGE TO UDP
+    #Loops through `ipList` and sends out the built packet to each IP in the list
     def broadcast(self,data,iplist):
         try:
             print("Sending beat...")
@@ -100,6 +119,7 @@ class heartbeatServer:
         except Exception as e:
             print("Error: ", e)
 
+    #Encapsulates the "beat". Initializes the zServer, builds the packet, broadcasts it, then waits 2 minutes until next iteration. 
     def beat(self,port,ipList):
         while True:
             serv = zServer()
@@ -108,9 +128,10 @@ class heartbeatServer:
             self.broadcast(dat,ipList)
             time.sleep(5)         #Wait 2 minutes until next beat
 
-
-
-
+#
+#   The heartbeat client.
+#   Recieves the heartbeat and launches the method that deals with the recieved data
+#
 class heartbeatClient:
     def __init__(self):
         self.port = 10009
@@ -120,7 +141,8 @@ class heartbeatClient:
  
         if len(addrs) == 0:
             raise Exception("there is no IPv6 address configured for localhost")
-        
+
+    #Gets the IPv6 address of the CJDNS device        
     def getAddr(self):
         me=ni.ifaddresses('tun0')[10][0]['addr']
         addrs = socket.getaddrinfo("localhost", self.port, socket.AF_INET6, 0, socket.SOL_TCP)
@@ -130,21 +152,23 @@ class heartbeatClient:
         tup[0] = me
         tup = tuple(tup)
         return tup
-
+    
+    #Function that gets called when a "beat" is recieved
     def updateHeartbeat(self, data, address):
         print("Heartbeat recieved from: ", address, data.decode())
 
+    #Listens for the heartbeat
     def getBeat(self, port):
         try:
             while True:
                 print("Listening")
                 client = zClient()
-                #print("Heartbeat recieved from: ", addr, output.decode())
                 output, addr = client.listen(self.getAddr(),10009)
                 self.updateHeartbeat(output, addr)
         except Exception as e:
             print("Error: ", e)
 
+    #Starts the client on a seperate thread
     def start(self,port):
         t = threading.Thread(target=self.getBeat,args=(port,))
         t.start()
